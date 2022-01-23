dfa57556b1c58274dc554f6f88d324f9861679dd
Ritwik Rajput <76650941+kaneki-ken260@users.noreply.github.com>
2022-01-23 14:33:20 +0530

    Update Maximum_sum_subarray_of_size_k.cpp

---
+++ Sliding Window/Maximum_sum_subarray_of_size_k.cpp
@@ -0,0 +1,39 @@
+/*
+
+Problem: Maximum subarray sum of size k.
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+    int p=0,j=0;
+    int n;cin>>n;
+    vector <int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int x;cin>>x;v.push_back(x);
+    }
+    int k;cin>>k;
+    int sum=0,mx=INT_MIN;
+    while (j<v.size())
+    {
+        sum+=v[j];
+        if(j-i+1<k)        // If the current window size is less than the required window size.
+        j++;
+
+        else if(j-i+1==k)
+        {
+            mx=max(mx,sum);
+            sum=sum-v[i];
+            i++;
+            j++;
+        }
+    }
+    cout<<mx;
+    return 0;
+}
